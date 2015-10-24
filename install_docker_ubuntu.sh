0811d20cc659e9c51865cbfbb7f2c3ba2cda3f74
baloran <contact@baloran.fr>
2015-10-24 11:27:03 +0200

    sudo everywhere

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -2,7 +2,7 @@
 
 echo "Install of docker is starting.."
 
-apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
 sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
 
