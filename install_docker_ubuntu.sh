9fd69cada50f8cf98c6f5e2861570d5823a2ee25
baloran <contact@baloran.fr>
2015-11-05 03:45:32 +0100

    update sudo

--- install_docker_ubuntu.sh
+++ install_docker_ubuntu.sh
@@ -4,7 +4,7 @@ echo "Install of docker is starting.."
 
 sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-sudo echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+sudo bash -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list'
 
 sudo apt-get purge lxc-docker*
 
